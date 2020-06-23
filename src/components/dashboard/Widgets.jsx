c93eaf625c963e675515a15feab3a1c25bd4043a
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-06-24 02:24:53 +0530

    change welcome page

--- src/components/dashboard/Widgets.jsx
+++ src/components/dashboard/Widgets.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-const Widget = () => <div> Hello User </div>;
+const Widget = () => (
+  <div>
+    <div className="container">
+      <div className="card card-body mt-5 text-center">
+        <h1 className="display-1">Admin Dashboard</h1>
+      </div>
+    </div>
+  </div>
+);
 
 export default Widget;
